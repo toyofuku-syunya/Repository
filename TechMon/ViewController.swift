79cdb4371930588e02cbdc8472b50df13ec751b9
豊福駿也 <SyunyaToyofuku@fengfumacbook.local>
2017-01-13 20:18:30 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by 豊福駿也 on 2017/01/13.
+//  Copyright © 2017年 Syunya Toyofuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
